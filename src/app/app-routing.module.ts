0139ab41dfce20e22731519479900ad24b82df5f
MaticDejan <matic.dejan.andrei@gmail.com>
2022-05-14 22:03:50 +0300

    MOVREV-3 Created login and forgotPassword components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SignupComponent } from './signup/signup.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
-
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'verify-email', component: VerifyEmailComponent },
                      // catch-all in case no other path matched
