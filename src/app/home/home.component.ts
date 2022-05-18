a4acb71efc5cee586e5659d37db72811e1c3877b
MaticDejan <matic.dejan.andrei@gmail.com>
2022-05-19 01:58:13 +0300

    Solved connection issues between login and navbar components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import {AuthService} from "../services/auth.service";
 
 @Component({
     selector: 'app-home',
@@ -12,7 +13,7 @@ import { Observable } from 'rxjs';
 export class HomeComponent implements OnInit {
 
 
-    constructor(public afAuth: AngularFireAuth, private firestore: AngularFirestore) {
+    constructor(public afAuth: AngularFireAuth, public authService: AuthService) {
 
     }
 
