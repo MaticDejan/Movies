309de53f63360c4ec92f5943b756ad4b12009054
Edi Lata <eduard.lata@student.upt.ro>
2022-05-21 21:22:12 +0300

    Added a safe pipe to ensure that the trailer link is safe

---
+++ src/app/safe.pipe.ts
@@ -0,0 +1,16 @@
+// Angular
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
+
+/**
+ * Sanitize HTML
+ */
+@Pipe({
+    name: 'safe'
+})
+export class SafePipe implements PipeTransform {
+    constructor(private sanitizer: DomSanitizer) { }
+    transform(url) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+}
